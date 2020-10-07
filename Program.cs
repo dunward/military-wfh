58910a6f0e4c6a2738bad41025f26a203a661c4f
weisswolfi <contact@wolfi.in>
2020-10-07 21:43:12 +0900

    project: reset project files to .net core console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace military_wfh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
