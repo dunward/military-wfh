4d5c984418d6c7f07ec1dcce7ed81b5e21aa4789
weisswolfi <contact@wolfi.in>
2020-10-08 00:00:07 +0900

    refactor: add pdf generator class

---
+++ sources/PdfGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace military_wfh
+{
+    public class PdfGenerator
+    {
+        private MilitaryPdfInfo info;
+
+        private PdfDocument document;
+        private PdfPage page;
+
+        private XGraphics graphics;
+        private XFont font;
+
+        private readonly double[] datePivots = { 365, 410, 451, 491, 529 };
+        private readonly double[] timePivots = { 352, 397, 439, 479, 518 };
+        private readonly double[] descriptionPivots = { 365, 410, 451, 491, 529 };
+
+        public PdfGenerator(MilitaryPdfInfo info)
+        {
+            this.info = info;
+
+            document = PdfReader.Open("template.pdf");
+            page = document.Pages[0];
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            graphics = XGraphics.FromPdfPage(page);
+            font = new XFont("Malgun Gothic", 9);
+        }
+
+        public void SaveGeneratePdfFile()
+        {
+            DrawSoldierInformation();
+            DrawWorkInfoTables();
+            DrawBottomTable();
+
+            document.Save($"{DateTime.Now:yyyy-MM-dd-ss}.pdf");
+        }
+
+        private void DrawSoldierInformation()
+        {
+            graphics.DrawString(info.Solider.Name, font, XBrushes.Black, new XPoint(170, 147));
+            graphics.DrawString(info.Solider.Birth, font, XBrushes.Black, new XPoint(310, 147));
+            graphics.DrawString(info.Solider.StartDate, font, XBrushes.Black, new XPoint(455, 147));
+            graphics.DrawString(info.Solider.Division, font, XBrushes.Black, new XPoint(250, 173));
+            graphics.DrawString(info.Solider.PhoneNumber, font, XBrushes.Black, new XPoint(280, 245));
+            graphics.DrawString(info.Solider.WorkingDate, font, XBrushes.Black, new XPoint(485, 245), XStringFormats.BaseLineCenter);
+            graphics.DrawString(info.Solider.Address, font, XBrushes.Black, new XPoint(170, 280));
+        }
+
+        private void DrawWorkInfoTables()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                graphics.DrawString(info.Solider.WorkInfos[i].Date, font, XBrushes.Black, new XPoint(108, datePivots[i]), XStringFormats.BaseLineCenter);
+                graphics.DrawString(info.Solider.WorkInfos[i].StartTime, font, XBrushes.Black, new XPoint(222, timePivots[i]));
+                graphics.DrawString(info.Solider.WorkInfos[i].EndTime, font, XBrushes.Black, new XPoint(222, timePivots[i] + 18.5));
+                graphics.DrawString(info.Solider.WorkInfos[i].Description, font, XBrushes.Black, new XPoint(437, descriptionPivots[i]), XStringFormats.BaseLineCenter);
+            }
+        }
+
+        private void DrawBottomTable()
+        {
+            graphics.DrawString(DateTime.Now.Month.ToString(), font, XBrushes.Black, new XPoint(415, 633), XStringFormats.BaseLineRight);
+            graphics.DrawString(DateTime.Now.Day.ToString(), font, XBrushes.Black, new XPoint(445, 633), XStringFormats.BaseLineRight);
+
+            graphics.DrawString(info.Solider.Name, font, XBrushes.Black, new XPoint(380, 657), XStringFormats.BaseLineCenter);
+            graphics.DrawString(info.CeoName, font, XBrushes.Black, new XPoint(380, 681), XStringFormats.BaseLineCenter);
+        }
+    }
+}
--- sources/Program.cs
+++ sources/Program.cs
@@ -14,52 +14,10 @@ namespace military_wfh
         static void Main(string[] args)
         {
             var obj = JObject.Parse(File.ReadAllText(@"data.json"));
-            MilitaryPdfInfo pdfInfo = new MilitaryPdfInfo(obj);
+            MilitaryPdfInfo info = new MilitaryPdfInfo(obj);
 
-            PdfDocument pdf = PdfReader.Open("template.pdf");
-            PdfPage page = pdf.Pages[0];
-
-
-            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-
-            XGraphics g = XGraphics.FromPdfPage(page);
-            XFont font = new XFont("Malgun Gothic", 9);
-
-            g.DrawString(pdfInfo.Solider.Name, font, XBrushes.Black, new XPoint(170, 147));
-            g.DrawString(pdfInfo.Solider.Birth, font, XBrushes.Black, new XPoint(310, 147));
-            g.DrawString(pdfInfo.Solider.StartDate, font, XBrushes.Black, new XPoint(455, 147));
-            g.DrawString(pdfInfo.Solider.Division, font, XBrushes.Black, new XPoint(250, 173));
-            g.DrawString(pdfInfo.Solider.PhoneNumber, font, XBrushes.Black, new XPoint(280, 245));
-            g.DrawString(pdfInfo.Solider.WorkingDate, font, XBrushes.Black, new XPoint(485, 245), XStringFormats.BaseLineCenter);
-            g.DrawString(pdfInfo.Solider.Address, font, XBrushes.Black, new XPoint(170, 280));
-
-            g.DrawString(pdfInfo.Solider.WorkInfos[0].Date, font, XBrushes.Black, new XPoint(108, 365), XStringFormats.BaseLineCenter);
-            g.DrawString(pdfInfo.Solider.WorkInfos[1].Date, font, XBrushes.Black, new XPoint(108, 410), XStringFormats.BaseLineCenter);
-            g.DrawString(pdfInfo.Solider.WorkInfos[2].Date, font, XBrushes.Black, new XPoint(108, 451), XStringFormats.BaseLineCenter);
-            g.DrawString(pdfInfo.Solider.WorkInfos[3].Date, font, XBrushes.Black, new XPoint(108, 491), XStringFormats.BaseLineCenter);
-            g.DrawString(pdfInfo.Solider.WorkInfos[4].Date, font, XBrushes.Black, new XPoint(108, 529), XStringFormats.BaseLineCenter);
-
-            var pivots = new double[] { 352, 397, 439, 479, 518 };
-
-            for (int i = 0; i < 5; i++)
-            {
-                g.DrawString(pdfInfo.Solider.WorkInfos[i].StartTime, font, XBrushes.Black, new XPoint(222, pivots[i]));
-                g.DrawString(pdfInfo.Solider.WorkInfos[i].EndTime, font, XBrushes.Black, new XPoint(222, pivots[i] + 18.5));
-            }
-
-            g.DrawString(pdfInfo.Solider.WorkInfos[0].Description, font, XBrushes.Black, new XPoint(437, 365), XStringFormats.BaseLineCenter);
-            g.DrawString(pdfInfo.Solider.WorkInfos[1].Description, font, XBrushes.Black, new XPoint(437, 410), XStringFormats.BaseLineCenter);
-            g.DrawString(pdfInfo.Solider.WorkInfos[2].Description, font, XBrushes.Black, new XPoint(437, 451), XStringFormats.BaseLineCenter);
-            g.DrawString(pdfInfo.Solider.WorkInfos[3].Description, font, XBrushes.Black, new XPoint(437, 491), XStringFormats.BaseLineCenter);
-            g.DrawString(pdfInfo.Solider.WorkInfos[4].Description, font, XBrushes.Black, new XPoint(437, 529), XStringFormats.BaseLineCenter);
-
-            g.DrawString(DateTime.Now.Month.ToString(), font, XBrushes.Black, new XPoint(415, 633), XStringFormats.BaseLineRight);
-            g.DrawString(DateTime.Now.Day.ToString(), font, XBrushes.Black, new XPoint(445, 633), XStringFormats.BaseLineRight);
-
-            g.DrawString(pdfInfo.Solider.Name, font, XBrushes.Black, new XPoint(380, 657), XStringFormats.BaseLineCenter);
-            g.DrawString(pdfInfo.CeoName, font, XBrushes.Black, new XPoint(380, 681), XStringFormats.BaseLineCenter);
-
-            pdf.Save("test.pdf");
+            PdfGenerator generator = new PdfGenerator(info);
+            generator.SaveGeneratePdfFile();
         }
     }
 }
